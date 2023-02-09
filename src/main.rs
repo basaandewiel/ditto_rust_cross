079f8e9ee8f4214782b2168a028e91126c0e3de6
Bas aan de Wiel <bas.aan.de.wiel@gmail.com>
2023-02-09 21:40:46 +0100

    removed static compilation build option

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ fn main() -> Result<(), DittoError> {
     .with_root(Arc::new(PersistentRoot::from_current_exe()?))
     .with_identity(|ditto_root| {
         // Provided as an env var, may also be provided as hardcoded string
-        let app_id = AppId::from_env("0219fe97-ccff-4b24-99d2-140f54ba9a60")?;
+        //let app_id  = "0219fe97-ccff-4b24-99d2-140f54ba9a60";
         let shared_token = "0aa8abeb-c950-4d5d-91da-e0a8fc9aa90a".to_string();
+        //let str1 = "0219fe97-ccff-4b24-99d2-140f54ba9a60";
+        //let app_id = AppId::as_str(str1 as AppId);
         //let app_id = AppId::from_env("0219fe97-ccff-4b24-99d2-140f54ba9a60")?;
+        let app_id = AppId::generate(); //@@@reading from env give run time error, NotPresent
+
         //let shared_token = std::env::var("0aa8abeb-c950-4d5d-91da-e0a8fc9aa90a").unwrap();
         let enable_cloud_sync = true;
         let custom_auth_url = None;
